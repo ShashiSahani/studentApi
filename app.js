464e547f4cab09d46445db73f6c73c4a777ae51f
ShashiSahani <shashisahani496@gmail.com>
2024-02-16 16:19:36 +0530

    passing id in param to check where the data is fetch throught id

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
-const express=require("express");
+const express = require("express");
 
-const app=express();
-const port =process.env.PORT || 3000;
-app.use(express.json())
-require("./db/connection")
-const Student=require('./models/students')
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+require("./db/connection");
+const Student = require("./models/students");
 
 // app.get("/",(req,res)=>{
 //     res.send("hello from get ok this")
 // })
 
-app.get("/", async(req,res)=>{
-    try {
-        //find({}) method to retrive all the student from mongodb
-        const students=await Student.find();
-        res.status(200).send(students)
-    } catch (error) {
-     res.status(500).send(error)   
-    }
-})
+app.get("/", async (req, res) => {
+  try {
+    //find({}) method to retrive all the student from mongodb
+    const students = await Student.find();
+    res.status(200).send(students);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 //create post request
-app.post("/students",async(req,res)=>{
-    try {
-        const student=new Student(req.body)
-        const createUser=await student.save();
-        res.status(201).send(createUser)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
+app.post("/students", async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    const createUser = await student.save();
+    res.status(201).send(createUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-app.listen(port,()=>{
-    console.log(`Connection is setup ${port}`)
-})
+app.get("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentData = await Student.findById(_id);
+    console.log(studentData)
+    if (!studentData) {
+      return res.status(404).send();
+    } else {
+      res.status(200).send(studentData);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+app.listen(port, () => {
+  console.log(`Connection is setup ${port}`);
+});
